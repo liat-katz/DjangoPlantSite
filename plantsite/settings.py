4a03b40c3e800505048cb590a4dd446412cf13b6
Liat Katz <liatkatz@Liats-MacBook-Pro.local>
2023-03-15 14:54:21 +0200

    add more static

--- plantsite/settings.py
+++ plantsite/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+    ] #folders that should be collected and considered when looking for static files
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
